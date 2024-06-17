98b3d4da1cb64ee6d40b2f7f4a01645ac9ea1fae
jizing <jizing@hotmail.com>
2024-06-18 04:06:11 +0800

    UUID _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f36e3da8-eb59-45d9-bb48-11f5d36d584d';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
